99323697d08e75e971c34c3282eb18c0c6651d38
Pete Nelson <nelsonp@aetna.com>
2019-03-01 15:14:08 -0700

    Intro done! Yay :)

--- src/components/sections/IntroSection.vue
+++ src/components/sections/IntroSection.vue
@@ -1,7 +1,11 @@
 <template>
   <div ref="section" class="section intro">
-    <div class="intro__card">
+    <div ref="introCard" class="intro__card">
       <PeteNelson ref="peteNelson" class="intro__pete-nelson"/>
+      <div ref="introCardSub" class="intro__card_sub">
+        <hr class="intro__card_bar" />
+        <span class="intro__card_text">Senior Developer & Designer</span>
+      </div>
     </div>
     <Cloud1 ref="cloud1" class="intro__cloud intro__cloud--1"/>
     <Cloud2 ref="cloud2" class="intro__cloud intro__cloud--2"/>
@@ -78,12 +82,21 @@ export default {
 
       if (firstLoad) {
         this.animatePeteNelson();
+      } else {
+        this.$refs.introCardSub.classList.add('intro__card_sub--show');
+        anime({
+          targets: this.$refs.introCard,
+          translateX: 0,
+          easing: 'easeOutSine',
+          duration: 400,
+        });
       }
 
       this.$refs.moon_back.classList.remove('intro__moon_back--hide');
       setTimeout(() => {
         this.$refs.moon_back.classList.add('intro__moon_back--show');
       }, 300);
+      console.log('showMoon', this.visible)
       anime({
         targets: this.$refs.moon,
         translateY: 0,
@@ -132,12 +145,22 @@ export default {
       this.$refs.moon_back.classList.remove('intro__moon_back--show');
       this.$refs.moon_back.classList.add('intro__moon_back--hide');
       anime({
-        targets: this.$refs.moon,
-        translateY: this.moonOffScreen(),
-        easing: 'easeInOutSine',
-        delay: 300,
+        targets: this.$refs.introCard,
+        translateX: this.introCardOffScreen(),
+        easing: 'easeOutSine',
         duration: 400,
       });
+      const self = this;
+      setTimeout(() => {
+        if (!self.visible) {
+          anime({
+            targets: this.$refs.moon,
+            translateY: this.moonOffScreen(),
+            easing: 'easeInOutSine',
+            duration: 400,
+          });
+        }
+      }, 300);
       anime({
         targets: this.$refs.cloud1,
         translateY: this.cloud1OffScreen(),
@@ -244,11 +267,14 @@ export default {
       });
       anime({
         targets: document.getElementById('name-mask-rectangle'),
-        width: '100%',
+        width: ['0%', '100%'],
         easing: 'easeInOutSine',
         delay: 500,
         duration: 500,
       });
+      setTimeout(() => {
+        this.$refs.introCardSub.classList.add('intro__card_sub--show');
+      }, 500);
     },
     skyLine1OffScreen() {
       return this.$refs.skyLine1.clientHeight + 50;
@@ -267,6 +293,9 @@ export default {
     },
     cloud2OffScreen() {
       return -1 * ((this.viewHeight * 0.19) + this.$refs.moon.clientHeight + 50);
+    },
+    introCardOffScreen() {
+      return -1 * (128 + this.$refs.introCard.clientWidth);
     },
     skyLine1Movement(offset) {
       return offset * (this.viewHeight / 15);
@@ -287,102 +316,6 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  @import "@/styles/_globals.scss";
-  .intro {
-    &__card {
-      position: absolute;
-      top: 8rem;
-      left: 8rem;
-      text-align: left;
-    }
-
-    &__pete-nelson {
-      height: 13rem;
-
-      .cls-1{
-        fill: $light-gray;
-      }
-      .cls-2,.cls-3,.cls-4{
-        fill: none;
-        stroke: $light-gray;
-        stroke-miterlimit: 10;
-      }
-      .cls-2{stroke-width:1.25rem;}
-      .cls-3{stroke-width:1rem;}
-      .cls-4{stroke-width:1.125rem}
-    }
-
-    &__cloud {
-      z-index: 2;
-      position: absolute;
-      width: 25%;
-      height: 25%;
-      opacity: .6;
-
-      & path {
-        fill: $dark-blue;
-      }
-
-      &--1 {
-        right: 25%;
-      }
-      &--2 {
-        top: 19%;
-        right: -3%;
-      }
-    }
-    &__moon {
-      z-index: 1;
-      position: absolute;
-      top: 10%;
-      right: 15%;
-      width: 20%;
-      height: 20%;
-    }
-    &__moon_back {
-      position: absolute;
-      width: 95%;
-      height: 95%;
-      top: -27.5%;
-      right: -22.5%;
-      opacity: 0;
-
-      &--hide {
-        opacity: 0;
-        transition: opacity linear 300ms;
-      }
-      &--show {
-        opacity: 1;
-        transition: opacity linear 300ms;
-      }
-    }
-    &__sky_line {
-      position: absolute;
-      right: 0;
-      bottom: -6vw;
-      width: 100%;
-    }
-    &__sky_line--1 {
-      z-index: 13;
-      & path {
-        fill: $dark-blue;
-      }
-    }
-    &__sky_line--2 {
-      z-index: 12;
-      bottom: -5.5vw;
-      & path {
-        fill: $darker-blue;
-      }
-    }
-    &__sky_line--3 {
-      z-index: 11;
-      bottom: -5vw;
-      & path {
-        fill: $darkest-blue;
-      }
-    }
-  }
+  @import "./introSection.scss";
 </style>
