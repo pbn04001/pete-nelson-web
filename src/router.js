20d5fe502944cdda366ef3ba5c6bbc77f8ef87b1
Pete Nelson <nelsonp@aetna.com>
2019-04-13 16:45:06 -0600

    Make resume not lazy load

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Resume from './views/Resume.vue';
 
 Vue.use(Router);
 
@@ -23,7 +24,8 @@ const createRouter = (store) => {
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ './views/Resume.vue'),
+        // component: () => import(/* webpackChunkName: "about" */ './views/Resume.vue'),
+        component: Resume,
       },
     ],
   });
